6121477197d83f08e90a3195dd76b13a3ab4e825
SalvatierraFran <franco14_93@hotmail.com>
2016-03-28 01:54:16 -0300

    Primer commit. Se crea la clase validacion.

---
+++ Validacion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio11
+{
+    class Validacion
+    {
+        public static bool Validar(int num, int num2, int num3)
+        {
+            if (num < num2 && num > num3)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
